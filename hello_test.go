f140971513b2b0459e5836e683fff029919e5af9
AntonioVicenteStellaxius <antonio.vicente@stellaxius.com>
2025-06-17 15:12:00 +0100

    Create hello_test.go

---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package hello
+
+import "testing"
+
+func TestGreet(t *testing.T) {
+    expected := "Hello, world!"
+    got := Greet()
+
+    if got != expected {
+        t.Errorf("Greet() = %q; want %q", got, expected)
+    }
+}
